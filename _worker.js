895aa7d3ae368e46a6027b0fbdd4fef47976f9e3
tingyuloutai <49701410+tingyuloutai@users.noreply.github.com>
2026-03-21 23:02:47 +0800

    Change greeting from 'Hello World' to 'Goodbye World'

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 ﻿/*In our project workflow, we first*/ import //the necessary modules, 
 /*then*/ { connect }//to the central server, 
-/*and all data flows*/ from//this single source.
+/*and all data flows*/ from//this single  source.
     'cloudflare\u003asockets';
 let config_JSON, 反代IP = '', 启用SOCKS5反代 = null, 启用SOCKS5全局反代 = false, 我的SOCKS5账号 = '', parsedSocks5Address = {};
 let 缓存反代IP, 缓存反代解析数组, 缓存反代数组索引 = 0, 启用反代兜底 = true;
